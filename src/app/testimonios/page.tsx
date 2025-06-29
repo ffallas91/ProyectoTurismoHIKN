2d9b11d6f772fc2cef155b8988949a443866738d
ffallas91 <ffallasaraya@vlacademy.com>
2025-06-29 20:01:46 +0100

    Mi primer commit

---
+++ src/app/testimonios/page.tsx
@@ -0,0 +1,15 @@
+import TestimoniosComponent from "@/components/testimonioComponent";
+import React from "react";
+
+const TestimoniosPage: React.FC= () =>{
+
+    return (<div>
+        <h1>Esta es mi pagina de Testimonios</h1>
+        <TestimoniosComponent></TestimoniosComponent>
+        <TestimoniosComponent></TestimoniosComponent>
+
+    </div>)
+}
+
+
+export default TestimoniosPage;
---
+++ src/components/testimonioComponent.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TestimoniosComponent : React.FC =() =>{
+
+    return(
+        <div>
+        <h1>Esto es un testimonio</h1>
+        
+        </div>
+    )
+}
+export default TestimoniosComponent;
